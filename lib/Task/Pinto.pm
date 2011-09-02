ea863cb7def76f2f9fcf773af0a8a7b0c3c2c030
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2011-09-02 21:48:45 +0000

    Import of THALJEF/Task-Pinto-0.001 from CPAN.
    gitpan-cpan-distribution: Task-Pinto
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         THALJEF/Task-Pinto-0.001.tar.gz
    gitpan-cpan-author:       THALJEF
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Pinto.pm
@@ -0,0 +1,143 @@
+package Task::Pinto;
+
+# ABSTRACT: All the Pinto components in one shot
+
+use strict;
+use warnings;
+
+#-------------------------------------------------------------------------------
+
+our $VERSION = '0.001'; # VERSION
+
+#-------------------------------------------------------------------------------
+1;
+
+
+
+=pod
+
+=for :stopwords Jeffrey Ryan Thalhammer Imaginative Software Systems cpan testmatrix url
+annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata
+placeholders
+
+=head1 NAME
+
+Task::Pinto - All the Pinto components in one shot
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+This Task installs all the Pinto components in one shot.  If you have
+created your own Pinto plugin, let me know so that I can add it here.
+
+=head1 CONTENTS
+
+=head2 L<Pinto>
+
+=head2 L<App::Pinto::Admin>
+
+=head2 L<Pinto::Remote>
+
+=head2 L<App::Pinto::Remote>
+
+=head2 L<Pinto::Server>
+
+=head2 L<Dist::Zilla::Plugin::ReleaseToPinto>
+
+=head1 SUPPORT
+
+=head2 Perldoc
+
+You can find documentation for this module with the perldoc command.
+
+  perldoc Task::Pinto
+
+=head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
+
+=over 4
+
+=item *
+
+Search CPAN
+
+The default CPAN search engine, useful to view POD in HTML format.
+
+L<http://search.cpan.org/dist/Task-Pinto>
+
+=item *
+
+RT: CPAN's Bug Tracker
+
+The RT ( Request Tracker ) website is the default bug/issue tracking system for CPAN.
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Pinto>
+
+=item *
+
+CPAN Ratings
+
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
+L<http://cpanratings.perl.org/d/Task-Pinto>
+
+=item *
+
+CPAN Testers
+
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
+
+L<http://www.cpantesters.org/distro/T/Task-Pinto>
+
+=item *
+
+CPAN Testers Matrix
+
+The CPAN Testers Matrix is a website that provides a visual way to determine what Perls/platforms PASSed for a distribution.
+
+L<http://matrix.cpantesters.org/?dist=Task-Pinto>
+
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=Task::Pinto>
+
+=back
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests by email to C<bug-task-pinto at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-Pinto>. You will be automatically notified of any
+progress on the request by the system.
+
+=head2 Source Code
+
+
+L<https://github.com/thaljef/Task-Pinto>
+
+  git clone git://github.com/thaljef/Task-Pinto.git
+
+=head1 AUTHOR
+
+Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Imaginative Software Systems.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
